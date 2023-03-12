4e09717a187b79f4424bfee4c8f233cb7f33badb
Daniel Zadok <zadok.daniel@gmail.com>
2023-03-12 21:46:46 +0200

    backg

---
+++ W060722ER - 2023.03.12/app1/src/App.jsx
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function SayHi() {
+  return <h1>hello</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <SayHi></SayHi>
+      <SayHi></SayHi>
+      <SayHi></SayHi>
+      <SayHi></SayHi>
+      <SayHi></SayHi>
+    </div>
+  );
+}
+
+export default App;
