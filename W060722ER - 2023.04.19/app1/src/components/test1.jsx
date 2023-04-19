d04d5b769cfd2deadf9bfbed711c7879f1cceeed
Daniel Zadok <zadok.daniel@gmail.com>
2023-04-19 18:47:53 +0300

    going around state with use ref

--- W060722ER - 2023.04.19/app1/src/components/test1.jsx
+++ W060722ER - 2023.04.19/app1/src/components/test1.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useRef, useState } from "react";
 
+
+
+
+
 const CIRCLE_SIZE = 50;
 
 const Test1 = () => {
-  const [lastPosition, setLastPosition] = useState(null);
+  //   const [lastPosition, setLastPosition] = useState(null);
   const myDiv = useRef();
   const circleDiv = useRef();
 
   useEffect(() => {
     const handler = (e) => {
-      setLastPosition({ x: e.clientX, y: e.clientY });
+      circleDiv.current.style.top = e.clientY - CIRCLE_SIZE / 2 + "px";
+      circleDiv.current.style.left = e.clientX - CIRCLE_SIZE / 2 + "px";
+      //   setLastPosition({ x: e.clientX, y: e.clientY });
     };
 
     const element = myDiv.current;
@@ -26,18 +32,19 @@ const Test1 = () => {
       ref={myDiv}
       className="top-0 left-0 min-vh-100 min-vw-100 bg-primary position-relative"
     >
-      {lastPosition ? (
-        <div
-          style={{
-            width: CIRCLE_SIZE,
-            height: CIRCLE_SIZE,
-            borderRadius: "50%",
-            top: lastPosition.y - CIRCLE_SIZE / 2,
-            left: lastPosition.x - CIRCLE_SIZE / 2,
-          }}
-          className="bg-danger position-absolute"
-        ></div>
-      ) : null}
+      {/* {lastPosition ? ( */}
+      <div
+        ref={circleDiv}
+        style={{
+          width: CIRCLE_SIZE,
+          height: CIRCLE_SIZE,
+          borderRadius: "50%",
+          // top: lastPosition.y - CIRCLE_SIZE / 2,
+          // left: lastPosition.x - CIRCLE_SIZE / 2,
+        }}
+        className="bg-danger position-absolute"
+      ></div>
+      {/* ) : null} */}
     </div>
   );
 };
