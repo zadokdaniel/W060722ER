2c2fbbcb25487a7440181614c0ab7189bc8faace
Daniel Zadok <zadok.daniel@gmail.com>
2023-09-03 21:23:59 +0300

    authentication autherization, card model, configs

---
+++ W060722WE - 2023.09.03/my-rest-api/routes/users.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+
+const { User, validateUser } = require("../models/users");
+const auth = require("../middleware/auth");
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password -__v");
+  res.json(user);
+});
+
+router.post("/", async (req, res) => {
+  // validate user's input
+  const { error } = validateUser(req.body);
+  if (error) {
+    res.status(400).json(error.details[0].message);
+    return;
+  }
+
+  // validate system
+  let user = await User.findOne({ email: req.body.email });
+  if (user) {
+    res.status(400).send("User already registered");
+    return;
+  }
+
+  // process
+  user = new User(req.body);
+  user.password = await bcrypt.hash(user.password, 12);
+
+  await user.save();
+
+  // results
+  res.json(_.pick(user, ["_id", "name", "email", "biz"]));
+});
+
+module.exports = router;
